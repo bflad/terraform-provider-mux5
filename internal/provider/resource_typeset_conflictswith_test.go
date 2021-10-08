03733ec30fafaaca87412ba13a90fb7bbd67ee68
Brian Flad <bflad417@gmail.com>
2021-10-08 11:06:21 -0400

    tests/resource_typeset_confictswith: Add random_pet for Computed value testing

--- internal/provider/resource_typeset_conflictswith_test.go
+++ internal/provider/resource_typeset_conflictswith_test.go
@@ -11,6 +11,12 @@ func TestAccResourceTypeSetConflictsWith(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"random": {
+				Source:            "hashicorp/random",
+				VersionConstraint: "3.1.0",
+			},
+		},
 		Steps: []resource.TestStep{
 			{
 				Config:      testAccResourceTypeSetConflictsBothAttrs,
@@ -19,25 +25,28 @@ func TestAccResourceTypeSetConflictsWith(t *testing.T) {
 			{
 				Config: testAccResourceTypeSetConflictsAttr1,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckTypeSetElemNestedAttrs("sdk_typeset_conflictswith.test", "example_typeset_block.*", map[string]string{
-						"example_attr1": "test1",
-					}),
+					resource.TestCheckTypeSetElemAttrPair(
+						"sdk_typeset_conflictswith.test", "example_typeset_block.*.example_attr1",
+						"random_pet.test", "id",
+					),
 				),
 			},
 			{
 				Config: testAccResourceTypeSetConflictsAttr2,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckTypeSetElemNestedAttrs("sdk_typeset_conflictswith.test", "example_typeset_block.*", map[string]string{
-						"example_attr2": "test2",
-					}),
+					resource.TestCheckTypeSetElemAttrPair(
+						"sdk_typeset_conflictswith.test", "example_typeset_block.*.example_attr2",
+						"random_pet.test", "id",
+					),
 				),
 			},
 			{
 				Config: testAccResourceTypeSetConflictsAttr1,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckTypeSetElemNestedAttrs("sdk_typeset_conflictswith.test", "example_typeset_block.*", map[string]string{
-						"example_attr1": "test1",
-					}),
+					resource.TestCheckTypeSetElemAttrPair(
+						"sdk_typeset_conflictswith.test", "example_typeset_block.*.example_attr1",
+						"random_pet.test", "id",
+					),
 				),
 			},
 		},
@@ -45,26 +54,32 @@ func TestAccResourceTypeSetConflictsWith(t *testing.T) {
 }
 
 const testAccResourceTypeSetConflictsAttr1 = `
+resource "random_pet" "test" {}
+
 resource "sdk_typeset_conflictswith" "test" {
   example_typeset_block {
-    example_attr1 = "test1"
+    example_attr1 = random_pet.test.id
   }
 }
 `
 
 const testAccResourceTypeSetConflictsAttr2 = `
+resource "random_pet" "test" {}
+
 resource "sdk_typeset_conflictswith" "test" {
   example_typeset_block {
-    example_attr2 = "test2"
+    example_attr2 = random_pet.test.id
   }
 }
 `
 
 const testAccResourceTypeSetConflictsBothAttrs = `
+resource "random_pet" "test" {}
+
 resource "sdk_typeset_conflictswith" "test" {
   example_typeset_block {
-    example_attr1 = "test1"
-    example_attr2 = "test2"
+    example_attr1 = random_pet.test.id
+    example_attr2 = random_pet.test.id
   }
 }
 `
